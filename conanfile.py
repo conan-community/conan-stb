14dffa1b7c377048d1c03a15a411fd438858023d
Luis Martinez de Bartolome <luism@jfrog.com>
2018-03-01 16:45:16 +0100

    license in licenses

--- conanfile.py
+++ conanfile.py
@@ -24,4 +24,4 @@ class StbConan(ConanFile):
 
     def package(self):
         self.copy("*.h", src="stb", dst="include")
-        self.copy("*why_public_domain.md", src="stb", dst="licenses")
+        self.copy("*why_public_domain.md", src="stb", dst="licenses", keep_path=False)
