a533b74b689c27a2849ae4ec7f71c1f770d80ce3
SSE4 <tomskside@gmail.com>
2018-08-29 11:59:46 +0700

    - define STB_TEXTEDIT_KEYTYPE, see https://github.com/nothings/stb/issues/493
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -19,9 +19,15 @@ class StbConan(ConanFile):
 
         if os.getenv("CONAN_RUN_TESTS", False):
             if not tools.os_info.is_windows:
+                tools.replace_in_file(os.path.join('stb', 'tests', 'Makefile'),
+                                      '-DSTB_DIVIDE_TEST',
+                                      '-DSTB_DIVIDE_TEST -DSTB_TEXTEDIT_KEYTYPE=unsigned')
                 with tools.chdir("stb/tests"):
                     self.run("make all")
 
     def package(self):
         self.copy("*.h", src="stb", dst="include")
         self.copy("*why_public_domain.md", src="stb", dst="licenses", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.defines.append('STB_TEXTEDIT_KEYTYPE=unsigned')
