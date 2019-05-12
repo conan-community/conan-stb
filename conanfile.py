89966f8dce6ac30d45c3c3dc8e48a4ec975447bf
SSE4 <tomskside@gmail.com>
2019-05-12 18:14:54 +0700

    - bump version to 20190512

--- conanfile.py
+++ conanfile.py
@@ -5,14 +5,14 @@ from conans import ConanFile, tools
 
 class StbConan(ConanFile):
     name = "stb"
-    version = "20180214"
+    version = "20190512"
     license = "Public domain"
     url = "https://github.com/conan-community/conan-stb"
     description = "stb single-file public domain libraries for C/C++ https://twitter.com/nothings"
     no_copy_source = True
 
     def source(self):
-        a_hash = {"20180214": "e6afb9c"}[str(self.version)]
+        a_hash = {"20190512": "1034f5e"}[str(self.version)]
         self.run("git clone https://github.com/nothings/stb.git")
         with tools.chdir("stb"):
             self.run("git reset --hard %s" % a_hash)
